9c37240ad61a0989bb3b7ff7d3cd034f44f4a18f
shajeen <shajeenahmed@gmail.com>
2019-09-10 11:33:54 +0530

    modified

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class XlntConan(ConanFile):
     url = "https://github.com/tfussell/xlnt"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = {"shared": True}
+    default_options = {"shared": False}
     generators = "cmake"
 
     def source(self):
@@ -29,9 +29,8 @@ class XlntConan(ConanFile):
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.so", dst="/usr/lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["xlnt"]
-
         self.cpp_info.cppflags = "-std=c++11"
